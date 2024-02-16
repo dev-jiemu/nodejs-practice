11baa37f851ff017a1d22f6b879c3ee1c363ba48
jiemu <rebianne03@naver.com>
2024-02-16 21:32:22 +0900

    2024.02.16 nodejs-practice - Web API Server : JWT authorization clone-coding

--- NodeBird-API/routes/v1.js
+++ NodeBird-API/routes/v1.js
@@ -1,7 +1,7 @@
 const express = require('express')
 
 const { verifyToken } = require('../middlewares')
-const { createToken, tokenTest } = require('../controllers/v1')
+const { createToken, tokenTest, getMyPosts, getPostByHashtag } = require('../controllers/v1')
 
 const router = express.Router()
 
@@ -10,5 +10,11 @@ router.post('/token', createToken)
 
 // v1/test GET
 router.get('/test', verifyToken, tokenTest)
+
+// v1/posts/my GET
+router.get('/posts/my', verifyToken, getMyPosts)
+
+// v1/posts/hashtag/:title GET
+router.get('/posts/hashtag/:title', verifyToken, getPostByHashtag)
 
 module.exports = router
--- Nodecat/routes/index.js
+++ Nodecat/routes/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
-const { test } = require('../controllers')
+const { searchByHashtag, getMyPosts, test } = require('../controllers')
 
 const router = express.Router()
 
 router.get('/test', test)
+
+router.get('/myposts', getMyPosts)
+router.get('/search/:hashtag', searchByHashtag)
 
 module.exports = router
