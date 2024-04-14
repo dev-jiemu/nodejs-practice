8347807e542c44a8f1f010cbf0c1013f836fe196
rebia <rebianne03@naver.com>
2024-04-14 22:34:16 +0900

    2024.04.14 nodejs-practice - API Server / Client clone-coding

---
+++ NodeBird/controllers/index.js
@@ -0,0 +1,45 @@
+const axios = require('axios')
+
+const URL = process.env.API_URL
+axios.defaults.headers.origin = process.env.ORIGIN
+
+const request = async (req, api) => {
+    try {
+        if(!req.session.jwt) {
+            const tokenResult = await axios.post(`${URL}/token`, {
+                clientSecret: process.env.CLIENT_SECRET
+            })
+            req.session.jwt = tokenResult.data.token
+        }
+
+        return await axios.get(`${URL}${api}`, {
+            heades: {authrization: req.session.jwt},
+        })
+    } catch (error) {
+        if (error.response?.status === 419) { // 토큰 만료라면 재발급 처리
+            delete req.session.jwt
+            return request(req, api)
+        }
+        throw error
+    }
+}
+
+exports.getMyPosts = async (req, res, next) => {
+    try {
+        const result = await request(req, '/posts/my')
+        res.json(result.data)
+    } catch (error) {
+        console.error(error)
+        next(error)
+    }
+}
+
+exports.searchByHashtag = async (req, res, next) => {
+    try {
+        const result = await request(req, `/posts/hashtag/${encodeURIComponent(req.params.hashtag)}`)
+        res.json(result.data)
+    } catch (error) {
+        console.error(error)
+        next(error)
+    }
+}
